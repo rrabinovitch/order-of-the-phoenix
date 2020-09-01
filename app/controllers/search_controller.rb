ca33c3f8b38af0bbc8392aaf0bcb02f68a1f8db8
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-01 10:22:51 -0600

    Connection works now

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,9 @@ class SearchController < ApplicationController
   def index
     house = params[:house]
 
-    conn = Faraday.new(url: 'https://www.potterapi.com/v1/characters') do |f|
-      f.params['key'] = ENV['HP-API-KEY']
-    end
+    conn = Faraday.new(url: 'https://www.potterapi.com/v1/characters')
 
-    response = conn.get("/characters?key=#{ENV['HP-API-KEY']}&house=#{house}&orderOfThePhoenix=true")
+    response = conn.get("?key=#{ENV['HP-API-KEY']}&house=#{house}&orderOfThePhoenix=true")
 
     members_info = JSON.parse(response.body, symbolize_names: true)
     binding.pry
