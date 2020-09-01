4db9c6d69992590aa67ae43d307093ae032cc133
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-01 10:47:54 -0600

    Define Member#initialize

--- app/poros/member.rb
+++ app/poros/member.rb
@@ -1,3 +1,8 @@
 class Member
-
+  def initialize(member_data)
+    @name = member_data[:name]
+    @role = member_data[:role]
+    @house = member_data[:house]
+    @patronus = member_data[:patronus]
+  end
 end
