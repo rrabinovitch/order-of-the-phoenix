e5d9f8e5e186d0429fed21b94bbe4907878b85cd
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-01 10:52:31 -0600

    Clean up SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,5 @@ class SearchController < ApplicationController
     house = params[:house]
     search = SearchFacade.new(house)
     @members = search.members
-    # conn = Faraday.new(url: 'https://www.potterapi.com/v1/characters')
-    #
-    # response = conn.get("?key=#{ENV['HP-API-KEY']}&house=#{house}&orderOfThePhoenix=true")
-    #
-    # @members = JSON.parse(response.body, symbolize_names: true)
   end
 end
