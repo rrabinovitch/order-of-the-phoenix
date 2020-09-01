ab33542a96a95cb93a3a2126fb07b6f9aed62cb6
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-01 10:20:57 -0600

    Set up Search#index action; connection unsuccessful so far

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,15 @@
 class SearchController < ApplicationController
   def index
+    house = params[:house]
 
+    conn = Faraday.new(url: 'https://www.potterapi.com/v1/characters') do |f|
+      f.params['key'] = ENV['HP-API-KEY']
+    end
+
+    response = conn.get("/characters?key=#{ENV['HP-API-KEY']}&house=#{house}&orderOfThePhoenix=true")
+
+    members_info = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+    # @members = members_info
   end
 end
